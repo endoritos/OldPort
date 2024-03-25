3f1e0a827a7fd98fa556a9449fa649f07f5f7a15
endoritos <endysempai@gmail.com>
2024-03-25 22:52:45 +0100

    Portfolio

---
+++ port.js
@@ -0,0 +1,11 @@
+document.querySelector('#Downloed').addEventListener('click', function() {
+  window.open("./cv.pdf", "_blank");
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#Hire').addEventListener('click', function() {
+    window.location.href = "mailto:Endysempai@gmail.com?subject=We love to set up a meeting with you";
+  });
+});
+
+
